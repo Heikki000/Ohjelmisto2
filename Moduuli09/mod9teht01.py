a6cdba4e41fd6f9ffd4cf2be3a8d1b43f589dee5
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-14 12:47:03 +0200

    valmis

--- Moduuli09/mod9teht01.py
+++ Moduuli09/mod9teht01.py
@@ -6,3 +6,22 @@ Uuden auton nopeus ja kuljetut matka on asetettava automaattisesti nollaksi.
 Kirjoita pääohjelma, jossa luot uuden auton (rekisteritunnus ABC-123, huippunopeus 142 km/h).
 Tulosta pääohjelmassa sen jälkeen luodun auton kaikki ominaisuudet.
 '''
+
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus_nyt = 0
+        self.matka = 0
+
+    def tiedot(self):
+        print(f"Auton rekisteritunnus on {self.rekisteritunnus}, huippunopeus {self.huippunopeus} km/h, "
+              f"tämänhetkinen nopeus {self.nopeus_nyt} km/h ja kuljettu matka {self.matka:.0f} km.")
+
+auto_1 = Auto("ABC-123", 145)
+
+auto_1.tiedot()
+
+# print(f"Auton rekisteritunnus on {auto_1.rekisteritunnus}") tämä muistutuksena, miten printataan suoraan
+
+
