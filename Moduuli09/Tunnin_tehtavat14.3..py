f3cea40806fe325112ed95a200dec044680b8789
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-14 11:14:19 +0200

    4

--- Moduuli09/Tunnin_tehtavat14.3..py
+++ Moduuli09/Tunnin_tehtavat14.3..py
@@ -1,24 +1,30 @@
 class Dog:       #luokka aloitetaan aina isolla kirjaimella
+
+    dog_count = 0
+
     def __init__(self, name, weight, age):
+        Dog.dog_count += 1
+        print(f"{Dog.dog_count}. Uusi koira luotu, nimi {name}")
         print(f"Uusi koira luotu, nimi: {name}")
         self.name = name
         self.weight = weight
         self.age = age
-        self.energyLevel = 50   # 0-100
+        self.energy_level = 500   # 0-100
         self.distance = 0   # meters
 
     def eat(self):
-        self.energyLevel = 100
+        self.energy_level = 1000
 
     def run(self, meters):
-        while self.energyLevel - meters * 0.1 >= 0:
-            self.distance = self.distance + meters
-            self.energyLevel = self.energyLevel - meters * 0.1
+        while meters > 0 and self.energy_level - 1 >= 0:
+            meters -= 1
+            self.distance = self.distance + 1
+            self.energy_level = self.energy_level - 1
 
     def say_hello(self):
         print(f"Hei, olen {self.name}, "
               f"paino: {self.weight} kg, ikä: {self.age} vuotta,"
-              f"energiaa jäljellä {self.energyLevel}, "
+              f"energiaa jäljellä {self.energy_level:.0f}, "
               f"juoksemani matka {self.distance} metriä.")
 
 
@@ -30,3 +36,6 @@ dog2.run(2000)
 dog1.say_hello()
 dog2.say_hello()
 
+dog3 = Dog("Muro", 12, 10)
+
+print(f"Koiria luotu yhteensä: {Dog.dog_count}")
