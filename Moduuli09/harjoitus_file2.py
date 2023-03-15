724153fa8330b607bf12ea8b488c7cb2c534b448
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-15 17:26:46 +0200

    15.3. esimerkki

--- Moduuli09/harjoitus_file2.py
+++ Moduuli09/harjoitus_file2.py
@@ -1,43 +1,42 @@
 import random
-
-
+from time import sleep
 class Auto:
-    auto_count = 0
+    def _init_(self, rekisteritunnus, huippunopeus):
+        self.register = rekisteritunnus
+        self.topspeed = huippunopeus
+        self.speed = 0
+        self.traveled = 0
 
-    def __init__(self, rekisteritunnus, huippunopeus):
-        Auto.auto_count += 1
-        print(f"{Auto.auto_count}. Uusi auto luotu, rekisteri: {rekisteritunnus} ja huippunopeus {huippunopeus}.")
-        self.rekisteritunnus = rekisteritunnus
-        self.huippunopeus = huippunopeus
-        self.nopeus = 0
-        self.matka = 0
+    def acceleration(self, speed):
 
-    def kiihdytä(self, muutos):
-        if muutos < 0:
-            while self.nopeus > 0 and muutos <= 0:
-                muutos += 1
-                self.nopeus -= 1
-        else:
-            while self.nopeus < self.huippunopeus and muutos > 0:
-                muutos -= 1
-                self.nopeus += 1
+        self.speed += speed
 
-    def kulje(self, aika):
-        if self.matka < 1000:
-            self.matka += aika * self.nopeus
+        if self.speed >= self.topspeed:
+            self.speed = self.topspeed
+        elif self.speed < 0:
+            self.speed = 0
 
-    def tiedot(self):
-        print(f"Auton rekisteritunnus on {self.rekisteritunnus}, huippunopeus {self.huippunopeus} km/h, "
-              f"tämänhetkinen nopeus {self.nopeus} km/h ja kuljettu matka {self.matka:.0f} km.")
+    def travel(self, hour):
+        self.traveled += self.speed * hour
 
 
-autot = [Auto("ABC-" + str(i + 1), random.randint(100, 200)) for i in range(10)]
+    pass
 
-for auto in autot:
-    while auto.matka < 1000:
-        auto.kiihdytä(random.randint(-10, 15))
-        auto.kulje(1)
-        auto.tiedot()
-        if auto.matka >= 1000:
-            print(f"Auto {auto.rekisteritunnus} on saavuttanut 1000 km:n matkan.")
+cars = []
+for n in range(1, 11):
+    car = Auto("ABC-"+str(n), random.randint(100,200))
+    cars.append(car)
+
+game= True
+while game == True:
+    for car in cars:
+        if car.traveled > 10000:
+            game = False
             break
+        else:
+            car.acceleration(random.randint(-10,15))
+            car.travel(1)
+
+
+for car in cars:
+    print(f"|{car.register:^{10}}|{car.topspeed:^{10}}|{car.speed:^{10}}|{car.traveled:^{10}}|")
