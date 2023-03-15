e36813bd946a6fb2a65862f0a36d8162431cd24a
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-15 20:32:59 +0200

    valmis

--- Moduuli10/mod10teht01.py
+++ Moduuli10/mod10teht01.py
@@ -7,12 +7,13 @@ Viimeksi mainitut metodit ajavat hissiä yhden kerroksen ylös- tai alaspäin ja
 missä kerroksessa hissi sen jälkeen on. Testaa luokkaa siten, että teet pääohjelmassa hissin
 ja käsket sen siirtymään haluamaasi kerrokseen ja sen jälkeen takaisin alimpaan kerrokseen.
 '''
-
+uusi_kerros = 0
 class Hissi:
     def __init__(self, alin, ylin):
         self.alin = alin
         self.ylin = ylin
-        self.kerros = 0
+        self.kerros = 1
+        self.uusi_kerros = uusi_kerros
 
     def kerros_ylös(self):
         self.kerros += 1
@@ -30,9 +31,11 @@ class Hissi:
         while self.kerros > uusi_kerros:
             self.kerros_alas()
         if self.kerros == uusi_kerros:
-            print(f"Olet heluamassasi kerroksessa, kerros {uusi_kerros:.0f}:ssa!")
+            #print(f"Olet haluamassasi kerroksessa, kerros {uusi_kerros:.0f}:ssa!")
+            return
 
 eka_hissi = Hissi(1, 7)
+print("Olet nyt talon alimmassa eli 1. kerroksessa. \n")
 
-eka_hissi.siirry_kerrokseen(3)
+eka_hissi.siirry_kerrokseen(int(input("Anna haluttu kerros (1-7): ")))
 eka_hissi.siirry_kerrokseen(1)
