9e6cd902d1747de1db4641168d8a120c990876a8
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-20 21:09:32 +0200

    20.3. valmis

--- Moduuli11/mod11teht01.py
+++ Moduuli11/mod11teht01.py
@@ -5,3 +5,38 @@ Kirjoita luokkiin myös tarvittavat alustajat. Tee aliluokkiin metodi tulosta_ti
 joka tudostaa kyseisen julkaisun kaikki tiedot. Luo pääohjelmassa julkaisut Aku Ankka (päätoimittaja Aki Hyyppä)
 ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua). Tulosta molempien julkaisujen kaikki tiedot toteuttamiesi metodien avulla.
 '''
+
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+    def tulosta_tiedot(self):
+        print(f"Julkaisun nimi on {self.nimi}.")
+
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjoittaja, sivumäärä):
+        self.kirjoittaja = kirjoittaja
+        self.sivumäärä = sivumäärä
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Kirjan on kirjoittanut {self.kirjoittaja} ja siinä on {self.sivumäärä} sivua.")
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, päätoimittaja):
+        self.päätoimittaja = päätoimittaja
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Lehden päätoimittaja on {self.päätoimittaja}.")
+
+lehti1 = Lehti('Aku Ankka', 'Aki Hyyppä')
+kirja1 = Kirja('Hytti n:o 6', 'Rosa Liksom', 200)
+
+julkaisut = [lehti1, kirja1]
+print()
+for j in julkaisut:
+    j.tulosta_tiedot()
+    print()
