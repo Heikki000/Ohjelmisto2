83990810d1246910224d980bc1eb7c67252587ee
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-20 15:34:42 +0200

    20.3. valmis

--- Moduuli10/mod10teht04.py
+++ Moduuli10/mod10teht04.py
@@ -15,36 +15,30 @@ jonka jälkeen aina tarkistetaan kilpailu_ohi-metodin avulla, onko kilpailu ohi.
 Ajantasainen tilanne tulostetaan tulosta tilanne-metodin avulla kymmenen tunnin välein sekä kertaalleen sen jälkeen, kun kilpailu on päättynyt.
 '''
 import random
-
+from time import sleep
 
 class Auto:
-    auto_count = 0
     def __init__(self, rekisteritunnus, huippunopeus):
-        Auto.auto_count += 1
-        #print(f"{Auto.auto_count}. Uusi auto luotu, rekisteri: {rekisteritunnus} ja huippunopeus {huippunopeus} km/h.")
         self.rekisteritunnus = rekisteritunnus
         self.huippunopeus = huippunopeus
         self.nopeus = 0
         self.matka = 0
 
     def kiihdytä(self, muutos):
-        if muutos < 0:
-            while self.nopeus > 0 and muutos <= 0:
-                muutos += 1
-                self.nopeus -= 1
-        else:
-            while self.nopeus < self.huippunopeus and muutos > 0:
-                muutos -= 1
-                self.nopeus += 1
+        self.nopeus += muutos
+        if self.nopeus < 0:
+            self.nopeus = 0
+        elif self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
 
     def kulje(self, aika):
-            self.matka = self.matka + aika * self.nopeus
-            return self.matka
+        self.matka = self.matka + aika * self.nopeus
+        return
 
 
-    def tiedot(self):
-        print(f"Auton rekisteritunnus on {self.rekisteritunnus}, huippunopeus {self.huippunopeus} km/h, "
-              f"tämänhetkinen nopeus {self.nopeus} km/h ja kuljettu matka {self.matka:.0f} km.")
+    #def tiedot(self):
+     #   print(f"Auton rekisteritunnus on {self.rekisteritunnus}, huippunopeus {self.huippunopeus} km/h, "
+      #        f"tämänhetkinen nopeus {self.nopeus} km/h ja kuljettu matka {self.matka:.0f} km.")
 
 class Kilpailu:
     def __init__(self, nimi, pituus, auto_lista):
@@ -68,10 +62,10 @@ class Kilpailu:
 
     def kilpailu_ohi(self):
         for auto in self.auto_lista:
-            if auto.matka >= self.pituus:
+            if auto.matka > self.pituus:
                 return True
-            else:
-                return False
+        else:
+            return False
 
 
 kilpailu = Kilpailu('Suuri romuralli', 8000, 10)
@@ -82,24 +76,8 @@ while True:
         kilpailu.tulosta_tilanne()
         break
     kilpailu.tulosta_tilanne()
+    sleep(0.5)
     for n in range(10):
         kilpailu.tunti_kuluu()
         if kilpailu.kilpailu_ohi() == True:
             break
-
-'''
-game = True
-while game == True:
-    for auto in autot:
-        if auto.matka >= 10000:
-            print("-------------------------------")
-            print(f"Auto {auto.rekisteritunnus} on saavuttanut 1000 km:n matkan ja voitti kilpailun!\n")
-            game = False
-            break
-        else:
-            auto.kiihdytä(random.randint(-10, 15))
-            auto.kulje(1)
-            #auto.tiedot()  testaamista varten
-print("|Rekisteritunnus|  Huippunopeus |  Loppunopeus  | Kokonaismatka |")
-for auto in autot:
-    print(f"|{auto.rekisteritunnus:^{15}}|{auto.huippunopeus:^{15}}|{auto.nopeus:^{15}}|{auto.matka:^{15}}|")'''
