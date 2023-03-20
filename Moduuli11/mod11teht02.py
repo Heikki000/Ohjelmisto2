3aaf41034d609453a60a8c5951569097364ae5c1
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-20 22:06:54 +0200

    20.3. valmis

--- Moduuli11/mod11teht02.py
+++ Moduuli11/mod11teht02.py
@@ -7,3 +7,50 @@ kahden ensin mainitun asettamiseksi sekä asettaa oman kapasiteettinsa. Kirjoita
 luot yhden sähköauton (ABC-15, 180 km/h, 52.5 kWh) ja yhden polttomoottoriauton (ACD-123, 165 km/h, 32.3 l).
 Aseta kummallekin autolle haluamasi nopeus, käske autoja ajamaan kolmen tunnin verran ja tulosta autojen matkamittarilukemat.
 '''
+
+
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        #self.nopeus = 0
+        self.matka = 0
+
+    def kiihdytä(self, muutos):
+        self.nopeus += muutos
+        if self.nopeus < 0:
+            self.nopeus = 0
+        elif self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+
+    def kulje(self, aika):
+        self.matka = self.matka + aika * self.nopeus
+        return
+
+    def tulosta_tilanne(self):
+        print("-----------------------------------------------------------------")
+        print(f"Auton {self.rekisteritunnus} mittarilukema on {self.matka} km.")
+
+class Sähkö(Auto):
+    def __init__(self, rekisteritunnus, huippunopeus, akkukapasiteetti):
+        self.akkukapasiteetti = akkukapasiteetti
+        self.nopeus = 100
+        super().__init__(rekisteritunnus, huippunopeus)
+
+class Polttomoottori(Auto):
+    def __init__(self, rekisteritunnus, huippunopeus, bensatankki):
+        self.bensatankki = bensatankki
+        self.nopeus = 80
+        super().__init__(rekisteritunnus, huippunopeus)
+
+
+s_auto1 = Sähkö('ABC-15', 180, 52.5)
+p_auto1 = Polttomoottori('ACD-123', 165, 32.3)
+
+autot = [s_auto1, p_auto1]
+
+for n in autot:
+    n.kulje(3)
+    n.tulosta_tilanne()
+
+
