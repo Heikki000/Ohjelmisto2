bedcb909d2e92bddc937988e8ebc097174ca6b16
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-20 13:03:38 +0200

    20.3. toimii

--- Moduuli10/mod10teht02.py
+++ Moduuli10/mod10teht02.py
@@ -28,7 +28,7 @@ class Hissi:
         while self.kerros > uusi_kerros:
             self.kerros_alas()
         if self.kerros == uusi_kerros:
-            print(f"Olet heluamassasi kerroksessa, kerros {uusi_kerros:.0f}:ssa!")
+            print(f"Hissi on haluamassasi kerroksessa, kerros {uusi_kerros:.0f}:ssa!\n")
             return
 
 class Talo:
@@ -41,7 +41,7 @@ class Talo:
             self.hissit.append(Hissi(alin, ylin))
 
     def aja_hissiä(self, hissi_nro, uusi_kerros):
-        self.hissi_nro = self.hissit[hissi - 1]
+        self.hissi_nro = self.hissit[-1]
         self.hissit[hissi_nro].siirry_kerrokseen(uusi_kerros)
 
 
@@ -49,15 +49,21 @@ uusi_kerros = 0
 talo = Talo(1, 7, 3)
 print("Tervetuloa ajelemaan talon hissejä. Olet nyt talon alimmassa eli 1. kerroksessa.")
 
-
-uusi_kerros = int(input("Mihin kerrokseen haluat lähettää hissin (1-7)? "))
-while uusi_kerros != '':
-    while uusi_kerros < 1 or uusi_kerros > 7:
-        print("Virhe: kerros on talon ulkopuolella.")
-        kohde = int(input("Mihin kerrokseen haluat lähettää hissin (1-7)? "))
-    hissi = int(input(f"Minkä hissin haluat valita (1-{len(talo.hissit)})? "))
-    talo.aja_hissiä(talo.hissit[hissi-1], uusi_kerros)
+hissi_nro = int(input(f"Minkä hissin haluat valita (1-{len(talo.hissit)})? "))
+while hissi_nro < 1 or hissi_nro > len(talo.hissit):
+    print("Virhe: Valitsit virheellisen hissinumeron, yritä uudelleen.")
+    hissi_nro = int(input(f"Minkä hissin haluat valita (1-{len(talo.hissit)})? "))
+while hissi_nro != '':
+    hissi_nro = int(hissi_nro) - 1
     uusi_kerros = int(input("Mihin kerrokseen haluat lähettää hissin (1-7)? "))
+    while uusi_kerros < 1 or uusi_kerros > 7:
+            print("Virhe: kerros on talon ulkopuolella.")
+            uusi_kerros = int(input("Mihin kerrokseen haluat lähettää hissin (1-7)? "))
+    talo.aja_hissiä(hissi_nro, uusi_kerros)
+    hissi_nro = input(f"Minkä hissin haluat valita (1-{len(talo.hissit)})? (tai paina ENTER lopettaaksesi) ")
+    if hissi_nro == '':
+        break
+
 
 print("Ohjelma lopetettu")
 
