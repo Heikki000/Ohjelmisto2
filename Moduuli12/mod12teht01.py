2e2f53646be2934b2ec47684a4bcf1bf40d3763f
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-21 10:58:02 +0200

    21.3. kesken

---
+++ Moduuli12/mod12teht01.py
@@ -0,0 +1,13 @@
+'''
+Kirjoita ohjelma, joka hakee ja tulostaa satunnaisen Chuck Norris -vitsin käyttäjälle.
+Käytä seuravalla sivulla esiteltävää rajapintaa: https://api.chucknorris.io/. Käyttäjälle on näytettävä pelkkä vitsin teksti.
+'''
+import json
+import requests
+
+pyyntö = "https://api.chucknorris.io/jokes/random"
+vastaus = requests.get(pyyntö).json()
+print(json.dumps(vastaus, indent=2))
+
+for a in vastaus:
+    print(a["value"])
