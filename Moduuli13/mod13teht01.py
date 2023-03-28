d681cff83e93c67d895f04e47ed0ebd5f67fa610
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-28 19:17:42 +0300

    28.3. aloitettu

--- Moduuli13/mod13teht01.py
+++ Moduuli13/mod13teht01.py
@@ -4,3 +4,22 @@ Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin.
 Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31.
 Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
 '''
+import json
+from flask import Flask, Response
+
+app = Flask(__name__)
+@app.route('/alkuluku/<luku>')
+def is_prime(luku):
+    try:
+        number = int(input("Anna luku: "))
+        is_prime = True
+
+        for n in range(2, number):
+            if number % n == 0:
+                print(f"{number} ei ole alkuluku.")
+                is_prime = False
+                break
+
+        # if is_prime == true:
+        if is_prime:
+            print(f"\n{number} on alkuluku.")
