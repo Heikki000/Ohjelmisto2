70a1ce84ad2f0ca2e8af9bc73fc033dc97c5e191
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-30 18:47:05 +0300

    30.3. toimii

--- Moduuli13/mod13teht02.py
+++ Moduuli13/mod13teht02.py
@@ -4,3 +4,40 @@ ja kaupungin JSON-muodossa. Tiedot haetaan opintojaksolla käytetystä lentokent
 Esimerkiksi EFHK-koodia vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/kenttä/EFHK.
 Vastauksen on oltava muodossa: {"ICAO":"EFHK", "Name":"Helsinki Vantaa Airport", "Municipality":"Helsinki"}.
 '''
+import json
+from flask import Flask, Response
+import mysql.connector
+
+def connect_db():
+    return mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='flight_game',
+        user='user1',
+        password='sala1',
+        autocommit=True
+    )
+connection = connect_db()
+
+app = Flask(__name__)
+@app.route('/kenttä/<haku_icao>')
+
+def get_airport(haku_icao):
+    query = f"select name, municipality from airport where ident ='{haku_icao}'"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchone()
+    if cursor.rowcount > 0:
+        vastaus = {'ICAO': haku_icao, 'Name': result[0], 'Municipality':result[1]}
+        return vastaus
+    else:
+        vastaus = {"palaute": "Ei tuloksia."}
+        return vastaus
+
+    get_airport(haku_icao)
+    vastaus = {"palaute": "Ei tuloksia."}
+    jsonvast = json.dumps(vastaus)
+    return Response(response=jsonvast, mimetype="application/json")
+
+if __name__== '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
